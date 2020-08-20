459d133c0205469a4d6010692c208e34a8ffc2e9
Mohammad-Ahsan-93 <67198788+Mohammad-Ahsan-93@users.noreply.github.com>
2020-08-20 13:47:43 +0500

    archive script1.js and create script.js
    Refactored requred field checked.

--- p01/script1.js
+++ p01/script1.js
@@ -25,27 +25,35 @@ function isValidEmail(email) {
     return re.test(String(email).toLowerCase());
 }
 
-// Function to check if required fields have data
-function checkRequired(inputArray) {
-   inputArray.forEach(function(input) {
-       if (input.value === '') {
-           showError(input,`${getFeild(input)} is required`);
-       } else {
-           showSucess(input);
-       }
-   });
-}
-
-// Function to get id of the inout field
-function getFeild(input) {
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-} 
-
 
 // This is an event listener for the form on submit button
 form.addEventListener('submit',function(e) {
     e.preventDefault();
     
-    checkRequired([username,email,password,password2]);
+    if (username.value === '') {
+        showError(username,'Username is required')
+    } else {
+        showSucess(username);
+    }
+
+    if (email.value === '') {
+        showError(email,'Email is required')
+    } else if (!isValidEmail(email.value)) {
+        showError(email,'Email is invalid')
+    } else {
+        showSucess(email);
+    }
+
+    if (password.value === '') {
+        showError(password,'Password is required')
+    } else {
+        showSucess(password);
+    }
+
+    if (password2.value === '') {
+        showError(password2,'Password is not matched with above password')
+    } else {
+        showSucess(password2);
+    }
 })
 
